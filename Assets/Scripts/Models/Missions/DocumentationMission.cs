3cd1e48071d9400eb5e295acccda209b8503ff10
Kai Siang Kao <work@ks-kao.com>
2024-10-06 19:33:13 +0800

    Made basic UI for resupply and documentation mission

--- Assets/Scripts/Models/Missions/DocumentationMission.cs
+++ Assets/Scripts/Models/Missions/DocumentationMission.cs
@@ -1,6 +1,9 @@
 public class DocumentationMission : Mission
 {
     private readonly LocationSO _destinationLocation = DataManager.Instance.GetRandomDestinationLocation();
+    private readonly NumberInput _resourceNumberInput = new("Resource");
+    private readonly NumberInput _supplyNumberInput = new("Supply");
+    private readonly NumberInput _paymentNumberInput = new("Payment");
     private int _numberOfResources = 0;
     private int _numberOfSupplies = 0;
     private int _numberOfPayments = 0;
@@ -11,10 +14,17 @@ public class DocumentationMission : Mission
 
     public override void OnDeploy()
     {
+        _numberOfResources = _resourceNumberInput.Value;
+        _numberOfSupplies = _supplyNumberInput.Value;
+        _numberOfPayments = _paymentNumberInput.Value;
     }
 
     protected override void GeneratePendingMissionUi()
     {
         base.GeneratePendingMissionUi();
+
+        PendingMissionUi.Add(_resourceNumberInput);
+        PendingMissionUi.Add(_supplyNumberInput);
+        PendingMissionUi.Add(_paymentNumberInput);
     }
 }
--- Assets/Scripts/Models/Missions/ResupplyMission.cs
+++ Assets/Scripts/Models/Missions/ResupplyMission.cs
@@ -1,6 +1,11 @@
+using UnityEngine.UIElements;
+
 public class ResupplyMission : Mission
 {
     private readonly TrainSO _train = DataManager.Instance.GetRandomTrain();
+    private readonly NumberInput _supplyNumberInput = new("Supply");
+    private readonly NumberInput _crewNumberInput = new("Crew");
+    private readonly NumberInput _resourceNumberInput = new("Resource");
     private int _numberOfSupplies = 0;
     private int _numberOfCrews = 0;
     private int _numberOfResources = 0;
@@ -11,10 +16,19 @@ public class ResupplyMission : Mission
 
     public override void OnDeploy()
     {
+        _numberOfSupplies = _supplyNumberInput.Value;
+        _numberOfCrews = _crewNumberInput.Value;
+        _numberOfResources = _resourceNumberInput.Value;
     }
 
     protected override void GeneratePendingMissionUi()
     {
         base.GeneratePendingMissionUi();
+
+        PendingMissionUi.Add(new Label(_train.name));
+
+        PendingMissionUi.Add(_supplyNumberInput);
+        PendingMissionUi.Add(_crewNumberInput);
+        PendingMissionUi.Add(_resourceNumberInput);
     }
 }
